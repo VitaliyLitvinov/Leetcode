e9533ef5de1157b9884f06e0193b5f6e463d9dbd
Vitaly_NB <v.litvinov2000@gmail.com>
2025-02-11 09:24:18 +0300

    xfgd

--- 1910.py
+++ 1910.py
@@ -1,12 +1,6 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        lst_part: list = list(part)
-        coeff: int = len(part)
-        lst: list = list(s)
-        while ''.join(lst_part) in ''.join(lst):
-            for idx in range(len(lst)):
-                if lst[idx:idx + coeff] == lst_part:
-                    del lst[idx:idx + coeff]
-                    break
-        return(''.join(lst))
+        while part in s:
+            s = s.replace(part, "", 1)
+        return s
 
