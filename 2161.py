d3575c54b5bbab8779bddf9c5bd8b8ee23d6f623
ubuntu <v.litvinov2000@gmail.com>
2025-03-03 08:38:42 +0300

    commit

---
+++ 2161.py
@@ -0,0 +1,15 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        less = []
+        more = []
+        same = []
+
+        for i in nums:
+            if i > pivot:
+                more.append(i)
+            elif i < pivot:
+                less.append(i)
+            else:
+                same.append(i)
+
+        return less + same + more
